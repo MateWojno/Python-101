2ea1c6e8e30d5573435d3540d29f21a884f65227
MateWojno <mateusz.k.wojno@gmail.com>
2022-08-05 12:00:55 +0200

    init calc

---
+++ 001.py
@@ -0,0 +1,14 @@
+# python calc
+# @MIT LIC - author MateWojno, mateusz.k.wojno@gmail.com
+a = input ('Give me number a:  ')
+b = input ('Give me number b: ')
+
+a = #convert to int
+b = #input converted to int
+
+c = #operation type
+
+#make UI with nice UX
+
+
+   
